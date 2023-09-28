2851a01bd6ae07ba3338439b6b4cfdd85efb6c4e
nilodude <alphone@protonmail.com>
2023-09-28 10:01:41 +0200

    add HUD & game start & game over

--- main.gd
+++ main.gd
@@ -7,17 +7,16 @@ var score
 func game_over():
 	$ScoreTimer.stop()
 	$MobTimer.stop()
+	$HUD.show_game_over()
 	
 	
 func new_game():
+	$HUD.update_score(score)
+	$HUD.show_message("Get Ready to be FUCKED")
 	get_tree().call_group(&"mobs", &"queue_free")
 	score = 0
 	$Player.start($StartPosition.position)
 	$StartTimer.start()
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	new_game()
 
 
 func _on_mob_timer_timeout():
@@ -47,6 +46,7 @@ func _on_mob_timer_timeout():
 
 func _on_score_timer_timeout():
 	score += 1
+	$HUD.update_score(score)
 
 
 func _on_start_timer_timeout():
